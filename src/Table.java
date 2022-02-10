8bd3bbb32fc5064e42336c380eb44d85e40db347
abdurrahman4127 <71093608+abdurrahman4127@users.noreply.github.com>
2022-02-10 17:41:33 +0600

    Initial commit

---
+++ src/Table.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Table
+{
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("multiplication table of : ");
+        int n = scanner.nextInt();
+
+        for (int i=1 ; i<=10; i++) {
+            System.out.printf("%d * %d = %d\n",n, i, n*i);
+        }
+    }
+}
